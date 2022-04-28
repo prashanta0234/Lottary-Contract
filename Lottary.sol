1d3a48f4d467917dcca039799c17a86ebbb41f59
"Prashanta Chakraborty" <"prashanta0234@gmail.com">
2022-04-28 20:56:58 +0600

    Smart contract

---
+++ Lottary.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0<0.9.0;
+
+contract Lottary{
+    address public manager;
+    address payable[] public participants;
+
+    constructor(){
+        manager=msg.sender;
+    }
+    receive() external payable{
+        require(msg.value==2 ether,"You have to pay 2 Ether");
+        participants.push(payable(msg.sender));
+    }
+    function checkBalance() public view returns(uint){
+        require(msg.sender==manager,"Sorry You are not Manager");
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+    function selectWiner() public{
+        require(participants.length>=3);
+        require(msg.sender==manager,"You are not the manager");
+        uint r=random();
+        address payable winner;
+        uint index= r% participants.length;
+        winner=participants[index];
+        winner.transfer(checkBalance());
+        participants=new address payable[](0);
+
+    }
+}
